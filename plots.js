aa9678a9916762fb2dd965f25340eb9707f385c0
Juan Pacheco <Juan.Pacheco@kingshawaiian.com>
2020-02-18 23:06:20 -0800

    Challenge

--- plots.js
+++ plots.js
@@ -35,9 +35,65 @@ function init() {
       PANEL.append("h6").text('Location: ' + result.location);
       PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
       PANEL.append("h6").text('WFREQ: ' + result.wfreq);
+
     });
   }
 
   function buildCharts(sample) {
-      console.log(sample)
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+
+      var samples = data.samples;
+      var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+      var yData = resultArray[0].otu_ids.slice(0,10)
+      var xData = resultArray[0].sample_values.slice(0,10)
+      var labels = resultArray[0].otu_labels.slice(0,10)
+      var trace = {
+        x: xData,
+        y: yData,
+        text: labels,
+        orientation: 'h',
+        width: 100,
+        type: 'bar'
+
+      };
+      Plotly.newPlot("bar", [trace]);
+
+      var trace1 = {
+        x: xData,
+        y: yData,
+        size: xData,
+        text: labels,
+        mode: 'markers'
+      };
+      var layout = {
+        title: 'Buble',
+        height: 600,
+        width: 500
+
+      };
+      Plotly.newPlot('bubble', [trace1], layout)
+
+      var trace1 = {
+        domain: {x: xData, y: yData},
+        value: result.wfreq,
+        title: {text: labels},
+        type: "indicator",
+        mode: 'gauge+number'
+      };
+    var layout = {
+      title: 'Bellly Button Washing Frequency',
+      height: 600,
+      width: 500
+
+    };
+    Plotly.newPlot('gauge', [trace1], layout)
+
+      console.log(resultArray[0]);
+      console.log('trace'+trace);
+      console.log('xdata'+xData);
+      console.log('ydata'+yData);
+    }); 
   }
